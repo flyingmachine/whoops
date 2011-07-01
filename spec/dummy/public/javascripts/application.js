64e12946b17b1699448603e59064ba2b00024644
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-07-01 08:36:33 -0400

    allowing dummy app to be used

--- spec/dummy/public/javascripts/application.js
+++ spec/dummy/public/javascripts/application.js
@@ -1,2 +1,35 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Whoops = {
+  setupFilters: function() {
+    $("#new_whoops_filter select").change(function(){
+      $("#new_whoops_filter").submit()
+    })
+    $("#reset").click(function(){
+      window.location = "/?whoops_filter[service]"
+      return false
+    })
+  },
+  
+  setupEventLinks: function() {
+    $("#instances a").click(function(){
+      $(".selected").removeClass("selected")
+      $(this).parents("li").addClass("selected")
+      $.get(this.href,function(data){
+        $("#event-details").html(data)
+      }, 'html')
+      return false;
+    })
+  },
+  
+  setup: function() {
+    this.setupFilters()
+    this.setupEventLinks();
+  }
+}
+
+$(function(){
+  Whoops.setup();
+})
+
+jQuery.ajaxSetup({ 
+  beforeSend: function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
