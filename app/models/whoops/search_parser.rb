b650370e0c4ec945a300ec593ac7c65298e7445d
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-07-03 12:10:05 -0400

    using yaml to parse values - handles regexps

--- app/models/whoops/search_parser.rb
+++ app/models/whoops/search_parser.rb
@@ -12,6 +12,7 @@ class Whoops::SearchParser
   
   def parse_line(line)
     key, method, value = line.match(/(.*?)(#.*)? (.*)/)[1..3]
+    
     key = key.to_sym
     method = method.sub(/^#/, '').to_sym
     value = parse_value(value)
@@ -24,26 +25,8 @@ class Whoops::SearchParser
   
   # Allows user to enter hashes or array
   def parse_value(value)
-    return JSON.parse(value)
-    case value.slice(0..0)
-    when '[': parse_array(value)
-    when '{': parse_hash(value)
-    else
-      parse_string(value)
-    end
-  end
-  
-  def parse_array(value)
-    array = value.gsub(/[\[\]]/, '').split(",")
-    array.collect{ |t| parse_string(t) }
-  end
-  
-  def parse_string(value)
-    if value =~ /^\d+$/
-      value.to_i
-    else
-      # remove beginning and ending quotes
-      value.sub(/^['"]/, '').sub(/['"$]/, '')
-    end
+    value = value.strip
+    value = "!ruby/regexp \"#{value}\"" if value =~ /^\/.*\/$/
+    return YAML.load(value)
   end
 end
--- spec/models/whoops/search_parser_spec.rb
+++ spec/models/whoops/search_parser_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe Whoops::SearchParser do
-  let(:search_parser){ Whoops::SearchParser.new("test") }
-  
+  describe "#mongoid_conditions" do
+    let(:search_parser){ Whoops::SearchParser.new("
+      details.backtrace#in 
+    ") }
+    it "correctly handles mehtods" do
+      
+    end
+  end
+
+  let(:search_parser){ Whoops::SearchParser.new("test") }  
   describe "#parse_line" do
     it "provides a key, method, and value when present" do
       parsed = search_parser.parse_line('details.backtrace#in ["Test"]')
@@ -13,15 +21,23 @@ describe Whoops::SearchParser do
   end
   
   describe "#parse_value" do
+    it "handles regular expressions" do
+      search_parser.parse_value('!ruby/regexp "/test/"').should == /test/
+    end
+    
+    it "handles regexp short form" do
+      search_parser.parse_value('/test/').should == /test/
+    end
+    
     it "handles numeric and string values in arrays" do
-      search_parser.parse_value('[1,"2",4]').should == [1,'2',4]
+      search_parser.parse_value('[1, "2", 4]').should == [1,'2',4]
     end
     
     it "handles hashes" do
-      search_parser.parse_value('{"1":3, "a":3, "a":"test"}').should == {
-        '1' => 3,
+      search_parser.parse_value('{1: 3, "a": 3, "1": "3"}').should == {
+        1 => 3,
         'a' => 3,
-        'a' => 'test'
+        '1' => '3'
       }
     end
   end
