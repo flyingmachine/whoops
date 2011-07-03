1fa52b2782a8692e8174fab2d830bf4585eb0957
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-07-03 11:10:39 -0400

    using json to handle value part

--- spec/models/whoops/search_parser_spec.rb
+++ spec/models/whoops/search_parser_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Whoops::SearchParser do
-  let(:sp){ Whoops::SearchParser.new("test") }
+  let(:search_parser){ Whoops::SearchParser.new("test") }
   
   describe "#parse_line" do
     it "provides a key, method, and value when present" do
-      parsed = sp.parse_line('details.backtrace#in ["Test"]')
+      parsed = search_parser.parse_line('details.backtrace#in ["Test"]')
       parsed[:key].should == "details.backtrace".to_sym
       parsed[:method].should == :in
       parsed[:value].should  == ["Test"]
@@ -14,7 +14,15 @@ describe Whoops::SearchParser do
   
   describe "#parse_value" do
     it "handles numeric and string values in arrays" do
-      sp.parse_value('[1,"2",4]').should == [1,'2',4]
+      search_parser.parse_value('[1,"2",4]').should == [1,'2',4]
+    end
+    
+    it "handles hashes" do
+      search_parser.parse_value('{"1":3, "a":3, a:"test"}').should == {
+        1 => 3,
+        'a' => 3,
+        'a' => 'test'
+      }
     end
   end
 end
