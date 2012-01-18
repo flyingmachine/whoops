759424f215c395fb6606d7c63c5b0cba52cb2d53
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-01-17 20:49:00 -0500

    remove old assets

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_view/railtie"
 require "action_mailer/railtie"
+require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
