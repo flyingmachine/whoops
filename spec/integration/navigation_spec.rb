651ef14b5a75a4d9676a77aa53a6ed55a314ab39
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-01-18 07:40:57 -0500

    checking that search works

--- spec/integration/navigation_spec.rb
+++ spec/integration/navigation_spec.rb
@@ -25,4 +25,20 @@ describe "Navigation" do
     visit whoops_event_group_events_path(e)
     page.should have_content("ArgumentError")
   end
+
+  it "should display event group with query" do
+    Whoops::Event.record(Whoops::Spec::ATTRIBUTES[:event_params])
+    e = Whoops::EventGroup.first
+    visit whoops_event_group_events_path(e, :query => "message#in ['ArgumentError']")
+    page.should have_content("ArgumentError")
+    #TODO use i18n
+    page.should_not have_content("Your search returned no results")
+  end
+
+  it "should not display event groups which don't match query" do
+    Whoops::Event.record(Whoops::Spec::ATTRIBUTES[:event_params])
+    e = Whoops::EventGroup.first
+    visit whoops_event_group_events_path(e, :query => "message#in ['Blorazrea']")
+    page.should have_content("Your search returned no results")
+  end
 end
