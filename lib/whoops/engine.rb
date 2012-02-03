8d51c24466a9e6732981ae0bab2494d3f692206a
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-02-03 11:31:23 -0500

    updated docs

--- lib/whoops/engine.rb
+++ lib/whoops/engine.rb
@@ -1,7 +1,7 @@
 module Whoops
   class Engine < Rails::Engine
     initializer "static assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, File.expand_path("../../../app/assets", __FILE__)
+      app.middleware.use ::ActionDispatch::Static, File.expand_path("../../../app/assets/javascripts", __FILE__)
     end
   end
 end
