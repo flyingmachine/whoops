3ea4c652dff966670d8ed39aad1902bc58da9b4c
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-07-27 12:21:12 -0400

    better header styling; breaking up home page into separate pages

--- doc/site/content/assets/javascripts/application.js
+++ doc/site/content/assets/javascripts/application.js
@@ -1,5 +1,11 @@
 $(function(){
-  $("#toc").toc({context: ".main", autoId: true})
+  var $win = $(window)
+    , $toc = $("#toc") 
+    , navTop = $('#toc').length && $('#toc').offset().top - 40
+    , isFixed = 0
+  
+  
+  $($toc).toc({context: ".main", autoId: true})
   $(".secondary").scrollspy()
   _.defer(function(){
     $('[data-spy="scroll"]').each(function () {
@@ -8,4 +14,17 @@ $(function(){
     $(".secondary .active").removeClass("active");
     $(".secondary #toc > ul > li:first-child").addClass("active");
   })
+
+
+  $win.on('scroll', processScroll)
+  function processScroll() {
+    var i, scrollTop = $win.scrollTop()
+    if (scrollTop >= navTop && !isFixed) {
+      isFixed = 1
+      $toc.addClass('toc-fixed')
+    } else if (scrollTop <= navTop && isFixed) {
+      isFixed = 0
+      $toc.removeClass('toc-fixed')
+    }
+  }
 })
