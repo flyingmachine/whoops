c7b5e4e06200e6b0971d96b67f74c44345afae74
Alex Rothenberg <alex@alexrothenberg.com>
2012-10-15 11:31:45 -0400

    added link to the event_notification email

--- app/mailers/whoops/notification_mailer.rb
+++ app/mailers/whoops/notification_mailer.rb
@@ -2,11 +2,16 @@ class Whoops::NotificationMailer < ActionMailer::Base
   def event_notification(event_group, addresses)
     @event_group = event_group
     @addresses = addresses
+    body = <<-BODY
+#{whoops_event_group_events_url(event_group.id)}
+
+#{event_group.service}: #{event_group.environment}: #{event_group.message}
+    BODY
     mail(
       :to      => addresses.join(", "),
       :from    => Rails.application.config.whoops_sender,
       :subject => "Whoops Notification | #{event_group.service}: #{event_group.environment}: #{event_group.message}",
-      :body    => "#{event_group.service}: #{event_group.environment}: #{event_group.message}"
+      :body    => body
     )
   end
 end
